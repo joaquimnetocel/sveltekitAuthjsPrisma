bd756702673418b9af3c541d628c0fdda2be2099
joaquimnetocel <joaquim.neto.cel@gmail.com>
2025-01-26 18:16:32 -0300

    PRISMA

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,7 @@
+// ADICIONADO POR MIM (CONFIGURAÇÃO DO PRISMA)
+import type { PrismaClient } from '@prisma/client';
+/////
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
@@ -8,6 +12,10 @@ declare global {
 		// interface PageState {}
 		// interface Platform {}
 	}
+	// ADICIONADO POR MIM (CONFIGURAÇÃO DO PRISMA)
+	// eslint-disable-next-line no-var
+	var prismaClient: PrismaClient;
+	/////
 }
 
 export {};
